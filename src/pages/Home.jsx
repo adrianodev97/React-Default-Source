6f492ec0ccfd932895a3d1ceb9b932cec3bebea5
Adriano Junior <vivaadrianonascimento@gmail.com>
2022-08-26 16:52:57 -0300

    Add Bootstrap Sass

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { util } from "../util/util"
 const Home = () => {
     return (
         <>
-            <h1 className="red"> {util.multi(5, 11)} </h1>
+            <h1 className="text-warning"> {util.multi(5, 11)} </h1>
             <div className="home">
                 <div className="containeir">
 
