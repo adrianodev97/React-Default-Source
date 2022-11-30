f298aa66572e612fb67cdd66a0b8f9b30a8d242c
Adriano Nascimento <adrianojunior.dev97@gmail.com>
2022-11-29 23:25:27 -0300

    Error Resolve

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,9 @@ import { util } from "../util/util"
 const Home = () => {
     return (
         <>
-            <h1 className="text-warning"> {util.multi(5, 11)} </h1>
             <div className="home">
-                <div className="containeir">
-
+                <div className="container">
+                    <h1 className="text"> {util.multi(5, 11)} </h1>
                 </div>
             </div>
         </>
